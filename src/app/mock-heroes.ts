e204a90daf3da5afbdb051aa0ce911d6f8c3be85
Plaznum <rycs1997@gmail.com>
2020-06-29 18:11:24 -0400

    Heroes and List Added

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Blaster Dark'},
+    { id: 12, name: 'Dragonic Overlord'},
+    { id: 13, name: 'Amaterasu'},
+    { id: 14, name: 'Tsukuyomi'},
+    { id: 15, name: 'Riviere'},
+    { id: 16, name: 'Nightmare Doll Alice'},
+    { id: 17, name: 'Crimson Impact, Metatron'},
+    { id: 18, name: 'Solidify Celestial, Zerachiel'},
+    { id: 19, name: 'Healthful Intendant'}
+
+];
